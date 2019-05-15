731e48d10ac3ca5dff72bde1bf3b4126b3e55713
JACrandall <50640321+JACrandall@users.noreply.github.com>
2019-05-15 16:05:36 -0500

    Add files via upload

---
+++ spacex_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Space-X API || author: janice.crandall@verizonwireless.com """
+import requests
+import pprint
+import webbrowser
+
+#access URL = https://api.spacexdata.com/v3/launches/latest
+resp = requests.get('https://api.spacexdata.com/v3/launches/latest')
+resp = resp.json()
+
+#pprint.pprint(resp)
+m_name = resp['mission_name']
+m_date = resp['launch_date_local']
+
+print("Name of the mission is: ", m_name)
+print("Launch Date: ", m_date)
+
+m_rock = resp["rocket"]["rocket_name"]
+print(m_rock, '\n')
+
+m_link = 'https://en.wikipedia.org/wiki/SpaceX_CRS-17'
+
+webbrowser.open(m_link)
+
